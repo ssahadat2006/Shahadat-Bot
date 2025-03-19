bb1f1d0ee3739334c5e3c32960a0fe1cbb542a3f
ssahadat2006 <prosahadat16@gmail.com>
2025-03-20 00:50:05 +0600

    Update ｢File｣__Info.js

--- Script/commands/｢File｣__Info.js
+++ Script/commands/｢File｣__Info.js
@@ -7,7 +7,7 @@ module.exports.config = {
     name: "admin",
     version: "1.0.0",
     hasPermssion: 0,
-    credits: "ullash",
+    credits: "Ullash",
     description: "Show Owner Info",
     commandCategory: "info",
     usages: "",
@@ -22,25 +22,25 @@ module.exports.run = async function({ api, event }) {
 ┏━━━━━━━━━━━━━━━━━━━━━┓
 ┃      🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟      
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 👤 𝐍𝐚𝐦𝐞      : 𝐮 𝐥 𝐥 𝐚 𝐬 𝐡 ッ
+┃ 👤 𝐍𝐚𝐦𝐞      : 𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐈𝐬𝐥𝐚𝐦
 ┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : 𝐌𝐚𝐥𝐞
-┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : 𝐈𝐧 𝐂𝐨𝐦𝐩𝐥𝐢𝐜𝐚𝐭𝐞𝐝
-┃ 🎂 𝐀𝐠𝐞       : 21
+┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : 𝐒𝐢𝐧𝐠𝐥𝐞
+┃ 🎂 𝐀𝐠𝐞       : 𝟏𝟖
 ┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
-┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 𝐝𝐢𝐩𝐥𝐨𝐦𝐚 𝐢𝐧 𝐀𝐠𝐫𝐢𝐜𝐮𝐥𝐭𝐮𝐫𝐞
-┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : 𝐍𝐨𝐚𝐤𝐡𝐚𝐥𝐢, 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
+┃ 🎓 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 𝐇𝐒𝐂 (𝟐𝟎𝟐𝟔)
+┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : 𝐌𝐚𝐡𝐚𝐥𝐜𝐡𝐚𝐫𝐢, 𝐊𝐡𝐚𝐠𝐫𝐚𝐜𝐡𝐡𝐚𝐫𝐢, 𝐂𝐡𝐢𝐭𝐭𝐚𝐠𝐨𝐧𝐠, 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : ullash01
-┃ 📢 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 : [𝐉𝐨𝐢𝐧 𝐍𝐨𝐰](https://t.me/mahbubu_shaon)
-┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : [𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞](https://www.facebook.com/profile.php?id=100000654976375)
+┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : [𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞(https://www.tiktok.com/@shahadat_bhai100k)
+┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : [𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞(https://www.facebook.com/2025sahadat2025)
+┃ ▶️ 𝐘𝐨𝐮𝐓𝐮𝐛𝐞 : [𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞(https://youtube.com/@shahadat-bai?)
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞: ${time}
 ┗━━━━━━━━━━━━━━━━━━━━━┛
         `,
-        attachment: fs.createReadStream(__dirname + "/cache/1.png")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
+        attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
+    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
   
-    return request(encodeURI(`https://graph.facebook.com/100000654976375/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
-        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
+    return request("https://i.imgur.com/7Wbb76O.jpeg")
+        .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
         .on('close', () => callback());
 };
